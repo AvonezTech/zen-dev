c3623ccdef8a2c99c5495666d241a623dd4f764d
umangapathak999 <umangapathak999@gmail.com>
2026-03-13 01:22:18 +0545

    Minor Update

--- app/Filament/Resources/PersonalBoards/Tables/PersonalBoardsTable.php
+++ app/Filament/Resources/PersonalBoards/Tables/PersonalBoardsTable.php
@@ -2,10 +2,15 @@
 
 namespace App\Filament\Resources\PersonalBoards\Tables;
 
+use App\Filament\Resources\PersonalBoards\Pages\PersonalBoardTaskBoard;
+use App\Models\PersonalBoard;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Enums\TextSize;
+use Filament\Tables\Columns\Layout\Grid;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -14,32 +19,30 @@ class PersonalBoardsTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->contentGrid([
+                'lg' => 2,
+            ])
             ->columns([
-                TextColumn::make('name')
-                    ->searchable(),
-                TextColumn::make('archived_at')
-                    ->dateTime()
-                    ->sortable(),
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                Grid::make([
+                    'md' => 2,
+                ])
+                    ->schema([
+
+                        TextColumn::make('name')
+                            ->searchable()
+                            ->size(TextSize::Large)
+                            ->weight(FontWeight::Bold),
+                    ]),
             ])
             ->filters([
                 //
             ])
-            ->recordActions([
-                ViewAction::make(),
-                EditAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
-            ]);
+            ->recordUrl(function (PersonalBoard $record) {
+                return PersonalBoardTaskBoard::getUrl([
+                    'record' => $record
+                ]);
+            })
+            ->recordActions([])
+            ->toolbarActions([]);
     }
 }
